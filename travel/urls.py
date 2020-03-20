e8ca9460cebd7a80baa48ef47f84c86c7dcdd300
chuckthemole <chuckthemole@gmail.com>
2020-03-20 15:29:17 -0700

    Deploying in Heroku

---
+++ travel/urls.py
@@ -0,0 +1,7 @@
+# Module 0
+from django.urls import path, include
+from . import views  #import everything from views module
+app_name = 'travel'
+urlpatterns = [
+    #path('',views.index, name='index'),
+]
