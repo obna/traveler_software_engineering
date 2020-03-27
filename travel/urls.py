003d2fcc9bc36fca5e9aab4e7198b4634f0230f0
Tania <tortizrosales@mills.edu>
2020-03-27 16:59:16 -0700

    authetication features added

--- travel/urls.py
+++ travel/urls.py
@@ -6,9 +6,9 @@ app_name = 'travel'
 urlpatterns = [
     path('',views.index, name='index'),
     path('dashboard', views.dashboard, name='dashboard'),
-    
+
     path('create', views.create, name='create'),
-    path("loguser", views.login_user, name="loguser"),
+    path('loguser', views.login_user, name='loguser'),
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('signup', views.signup, name='signup'),
--- travel/views.py
+++ travel/views.py
@@ -61,16 +61,57 @@ def dashboard(request):
             return render(request, "travel/dashboard.html")
 
 def create(request):
-    pass
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        coder_yet = request.POST['coder_yet_checkbox']
+
+        if username is not None and email is not None and password is not None: # checking that they are not None
+            if not username or not email or not password: # checking that they are not empty
+                return render(request, "share/signup.html", {"error": "Please fill in all required fields"})
+            if User.objects.filter(username=username).exists():
+                return render(request, "share/signup.html", {"error": "Username already exists"})
+            elif User.objects.filter(email=email).exists():
+                return render(request, "share/signup.html", {"error": "Email already exists"})
+            # save our new user in the User model
+            user = User.objects.create_user(username, email, password)
+            coder = Coder.objects.create(user= user, coder_yet = coder_yet).save()
+            user.save()
+
+            login(request, user, backend="django.contrib.auth.backends.ModelBackend")
+            # this logs in our new user, backend means that we are using the  Django specific auhentication and not 3rd party
+
+        return redirect("share:index")
+
+    else:
+        return redirect("share:signup")
 
 def signup(request):
-    pass
+    if request.user.is_authenticated:
+        return redirect("share:index")
+    return render(request, 'share/signup.html')
 
 def login_user(request):
-    pass
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        if not username or not password:
+            return render(request, "share/login.html", {"error":"One of the fields was empty"})
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("share:index")
+        else:
+            return render(request, "share/login.html", {"error":"Wrong username or password"})
+    else:
+        return redirect("share:index")
 
 def login_view(request):
-    pass
+    if request.user.is_authenticated:
+        return redirect("share:index")
+    return render(request, 'share/login.html')
 
 def logout_view(request):
-    pass
+    logout(request)
+    return redirect("share:login")
